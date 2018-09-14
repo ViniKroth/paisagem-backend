2995dcb077c0e2b57c104a8060cda5347e4d59f2
victoria <victoria.azevedo@acad.pucrs.br>
2018-09-13 21:43:23 -0300

    Método listagem de Especie camada Controller

--- Controllers/EspecieController.js
+++ Controllers/EspecieController.js
@@ -6,6 +6,23 @@ especieController.addEspecie = addEspecie;
 
 module.exports = especieController;
 
+class EspecieController {
+    /*  ROTAS DE FETCH:
+     *  Todas as rotas de fetch lidam com informações pertinentes a vários usuários
+     */
+    
+    //Fetch especie
+  static fetchEspecies(query, callback) {
+    console.log(callback);
+
+    const orderQuery = UserController.constructOrderQuery(query);
+    const whereQuery = UserController.constructWhereQuery(query);
+
+    return DAO.fetchEspecies(orderQuery, whereQuery, callback);
+  }
+
+}
+
 function addEspecie(req, res, next){
     daoEspecie.addEspecie(req.body, (error, especie) => {
         if(error){
@@ -17,3 +34,4 @@ function addEspecie(req, res, next){
         }
     });
 }
+
